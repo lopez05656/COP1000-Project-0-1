d6c1c36139c6f75f846d50ef0ee47125ae7b7cd4
lopez05656 <lopzac981@gmail.com>
2024-11-16 00:39:59 -0500

    Assignment Complete.

---
+++ Carfinder.py
@@ -0,0 +1,51 @@
+# Register Dependencies
+import time
+
+# Program Start
+
+def displayVehicles():
+ print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ")
+ file = open("availablevehicles.txt", "r")
+ fileContents = file.read()
+ print(fileContents)
+ print("Returning to the menu in 5 seconds")
+ time.sleep(1)
+ print("Returning to the menu in 4 seconds.")
+ time.sleep(1)
+ print("Returning to the menu in 3 seconds..")
+ time.sleep(1)
+ print("Returning to the menu in 2 seconds...")
+ time.sleep(1)
+ print("Returning to the menu in 1 seconds.")
+ time.sleep(1)
+ print("Returning to the menu now...")
+ time.sleep(1)
+ menu()
+
+def menu():
+ print("************************************")
+ print("  AutoCountry Vehicle Finder v0.1")
+ print("************************************")
+ print("Please select one of the following numbers from the menu below:")
+
+ print("1: PRINT all Authorized Vehicles")
+ print("2: Exit")
+ menuSelection = int(input("Your Selection: "))
+ 
+ if menuSelection == int("1"):
+  displayVehicles()
+
+ if menuSelection == int("2"):
+    print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+    exit()
+menu()
+ 
+
+if menuSelection == int("1"):
+ displayVehicles()
+
+if menuSelection == int("2"):
+    print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+    exit()
+
+# Program Complete. 
